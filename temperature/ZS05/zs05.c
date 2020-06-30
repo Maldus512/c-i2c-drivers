b69ad8ab08269f806df2dc6d927d10e77e9325cd
Mattia Maldini <mattia512maldini@gmail.com>
2020-06-30 08:56:43 +0200

    Added ZS05 driver (a bit hacky, the datasheet lies)

--- temperature/ZS05/zs05.c
+++ temperature/ZS05/zs05.c
@@ -1,9 +1,9 @@
 #include "i2c_common/i2c_common.h"
 
-#define HUMIDITY_INTEGER          0
-#define HUMIDITY_DECIMAL_PLACE    1
-#define TEMPERATURE_INTEGER       2
-#define TEMPERATURE_DECIMAL_PLACE 3
+#define HUMIDITY_INTEGER          2
+#define HUMIDITY_DECIMAL_PLACE    3
+#define TEMPERATURE_INTEGER       0
+#define TEMPERATURE_DECIMAL_PLACE 1
 #define CHECKSUM                  4
 
 #include <stdio.h>
@@ -11,9 +11,17 @@
 
 int zs05_read(i2c_driver_t driver, double *temperature, double *humidity) {
     uint8_t buffer[5] = {0};
-    int     res       = i2c_read_register(driver, HUMIDITY_INTEGER, buffer, 5);
 
-    printf("0x%02X 0x%02X 0x%02X 0x%02X 0x%02X\n", buffer[0], buffer[1], buffer[2], buffer[3], buffer[4]);
+    i2c_read_register(driver, HUMIDITY_INTEGER, &buffer[0], 1);
+    i2c_read_register(driver, HUMIDITY_DECIMAL_PLACE, &buffer[1], 1);
+    i2c_read_register(driver, TEMPERATURE_INTEGER, &buffer[2], 1);
+    i2c_read_register(driver, TEMPERATURE_DECIMAL_PLACE, &buffer[3], 1);
+    i2c_read_register(driver, CHECKSUM, &buffer[4], 1);
 
-    return res;
+    if (temperature)
+        *temperature = (double)buffer[2] + ((double)buffer[3]) * .2;
+    if (humidity)
+        *humidity = (double)buffer[0] + ((double)buffer[1]) * .2;
+
+    return !(buffer[0] + buffer[1] + buffer[2] + buffer[3] == buffer[4]);
 }
