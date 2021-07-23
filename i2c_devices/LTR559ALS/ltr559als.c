fcbd89493222ddd0da50e1f64276f91ee735f51a
Mattia Maldini <mattia512maldini@gmail.com>
2021-07-23 17:50:15 +0200

    Added more configuration functions

--- i2c_devices/LTR559ALS/ltr559als.c
+++ i2c_devices/LTR559ALS/ltr559als.c
@@ -2,33 +2,33 @@
 #include <stdint.h>
 #include "ltr559als.h"
 
-#define MANUFAC_ID 0x87
-#define PART_ID 0x86
-#define ALS_DATA_CH1_LOW 0x88
-#define ALS_CONTR 0x80
-#define PS_CONTR 0x81
-#define PS_DATA_0_LOW 0x8d
-#define INTERRUPT 0x8f
-#define PS_THRES_UP_0 0x90
-#define ALS_PS_STATUS 0x8c
-#define PS_MEAS_RATE 0x84
-#define ALS_MEAS_RATE 0x85
-#define INTERRUPT_PERSIST 0x9e
+#define ALS_CONTR         0x80
+#define PS_CONTR          0x81
+#define PS_MEAS_RATE      0x84
+#define ALS_MEAS_RATE     0x85
+#define PART_ID           0x86
+#define MANUFAC_ID        0x87
+#define ALS_DATA_CH1_LOW  0x88
+#define ALS_PS_STATUS     0x8C
+#define PS_DATA_0_LOW     0x8D
+#define INTERRUPT         0x8F
+#define PS_THRES_UP_0     0x90
+#define PS_OFFSET_0       0x94
+#define INTERRUPT_PERSIST 0x9E
 
 
-int ltr559als_read_info_id(i2c_driver_t driver, uint8_t *p_id, uint8_t *m_id)
-{
+int ltr559als_read_info_id(i2c_driver_t driver, uint8_t *p_id, uint8_t *m_id) {
     uint8_t buffer[2] = {0};
-    int res = i2c_read_register(driver, PART_ID, buffer, 2);
-    *p_id = buffer[0];
-    *m_id = buffer[1];
+    int     res       = i2c_read_register(driver, PART_ID, buffer, 2);
+    *p_id             = buffer[0];
+    *m_id             = buffer[1];
     return res;
 }
 
-int ltr559als_read_als_data(i2c_driver_t driver, uint16_t *ch_0, uint16_t *ch_1)
-{
+
+int ltr559als_read_als_data(i2c_driver_t driver, uint16_t *ch_0, uint16_t *ch_1) {
     uint8_t buffer[4] = {0};
-    int res = i2c_read_register(driver, ALS_DATA_CH1_LOW, buffer, 4);
+    int     res       = i2c_read_register(driver, ALS_DATA_CH1_LOW, buffer, 4);
     if (ch_1)
         *ch_1 = buffer[0] | (buffer[1] << 8);
     if (ch_0)
@@ -36,20 +36,21 @@ int ltr559als_read_als_data(i2c_driver_t driver, uint16_t *ch_0, uint16_t *ch_1)
     return res;
 }
 
+
 int ltr559als_read_visible_spectrum(i2c_driver_t driver, uint16_t *light) {
     uint16_t ch_1, ch_0;
-    int res=ltr559als_read_als_data(driver, &ch_0, &ch_1);
-    if (ch_0<ch_1) 
-        *light=0;
-    else 
-         *light=ch_0-ch_1;
+    int      res = ltr559als_read_als_data(driver, &ch_0, &ch_1);
+    if (ch_0 < ch_1)
+        *light = 0;
+    else
+        *light = ch_0 - ch_1;
     return res;
 }
 
-int ltr559als_als_standby(i2c_driver_t driver, int standby)
-{
+
+int ltr559als_als_standby(i2c_driver_t driver, int standby) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, ALS_CONTR, &buffer, 1);
+    int     res    = i2c_read_register(driver, ALS_CONTR, &buffer, 1);
     if (res)
         return res;
     if (standby == 0)
@@ -59,10 +60,10 @@ int ltr559als_als_standby(i2c_driver_t driver, int standby)
     return i2c_write_register(driver, ALS_CONTR, &buffer, 1);
 }
 
-int ltr559als_ps_standby(i2c_driver_t driver, int standby)
-{
+
+int ltr559als_ps_standby(i2c_driver_t driver, int standby) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, PS_CONTR, &buffer, 1);
+    int     res    = i2c_read_register(driver, PS_CONTR, &buffer, 1);
     if (res)
         return res;
     if (standby == 0)
@@ -72,11 +73,11 @@ int ltr559als_ps_standby(i2c_driver_t driver, int standby)
     return i2c_write_register(driver, PS_CONTR, &buffer, 1);
 }
 
-int ltr559als_read_ps_data(i2c_driver_t driver, uint16_t *value)
-{
+
+int ltr559als_read_ps_data(i2c_driver_t driver, uint16_t *value) {
     uint8_t buffer[2] = {0};
-    int res = i2c_read_register(driver, PS_DATA_0_LOW, buffer, 2);
-    *value = buffer[0] | ((buffer[1] & 0b111) << 8);
+    int     res       = i2c_read_register(driver, PS_DATA_0_LOW, buffer, 2);
+    *value            = buffer[0] | ((buffer[1] & 0b111) << 8);
     return res;
 }
 
@@ -84,149 +85,172 @@ int ltr559als_read_ps_data(i2c_driver_t driver, uint16_t *value)
 int ltr559als_set_interrupt_mode(i2c_driver_t driver, ltr559als_interrupt_mode_t mode) {
 
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, INTERRUPT, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xfc) | mode;
+    int     res    = i2c_read_register(driver, INTERRUPT, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xfc) | mode;
     i2c_write_register(driver, INTERRUPT, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, INTERRUPT, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, INTERRUPT, &buffer, 1);
     return 0;
 }
 
-int ltr559als_set_ps_thresold(i2c_driver_t driver, uint16_t low, uint16_t high) {
 
-    uint8_t buffer[4]={0};
-    buffer[2]=low & 0xff;
-    buffer[3]=(low >> 8) & 0b1111;
-    buffer[0]=high & 0xff;
-    buffer[1]=(high >> 8) & 0b1111;
+int ltr559als_set_ps_thresold(i2c_driver_t driver, uint16_t low, uint16_t high) {
+    uint8_t buffer[4] = {0};
+
+    buffer[2] = low & 0xff;
+    buffer[3] = (low >> 8) & 0b1111;
+    buffer[0] = high & 0xff;
+    buffer[1] = (high >> 8) & 0b1111;
 
     return i2c_write_register(driver, PS_THRES_UP_0, buffer, 4);
 }
 
-int ltr559als_read_status(i2c_driver_t driver, uint8_t *buffer) {
 
+int ltr559als_set_ps_offset(i2c_driver_t driver, uint16_t offset) {
+    uint8_t buffer[2] = {0};
+
+    buffer[0] = (offset & 0x300) >> 8;
+    buffer[1] = offset & 0xFF;
+
+    return i2c_write_register(driver, PS_OFFSET_0, buffer, 2);
+}
+
+
+int ltr559als_read_status(i2c_driver_t driver, uint8_t *buffer) {
     return i2c_read_register(driver, ALS_PS_STATUS, buffer, 1);
 }
 
 
 int ltr559als_set_als_gain(i2c_driver_t driver, ltr559als_als_gain_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, ALS_CONTR, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xe3) | (mode<<2);
+    int     res    = i2c_read_register(driver, ALS_CONTR, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xe3) | (mode << 2);
     i2c_write_register(driver, ALS_CONTR, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, ALS_CONTR, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, ALS_CONTR, &buffer, 1);
     return 0;
 }
 
+
 int ltr559als_ps_saturation_indicator_en(i2c_driver_t driver, int enable) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, PS_CONTR, &buffer, 1);
+    int     res    = i2c_read_register(driver, PS_CONTR, &buffer, 1);
     if (res)
         return res;
-    if (enable == 0) 
+    if (enable == 0)
         buffer = buffer & 0xdf;
     else if (enable == 1)
         buffer = buffer | 0x20;
     return i2c_write_register(driver, PS_CONTR, &buffer, 1);
 }
 
+
 int ltr559als_set_ps_meas_rate(i2c_driver_t driver, ltr559als_ps_meas_rate_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, PS_MEAS_RATE, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xf0) | mode;
+    int     res    = i2c_read_register(driver, PS_MEAS_RATE, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xf0) | mode;
     i2c_write_register(driver, PS_MEAS_RATE, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, PS_MEAS_RATE, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, PS_MEAS_RATE, &buffer, 1);
     return 0;
 }
-
 
 
 int ltr559als_set_als_meas_rate(i2c_driver_t driver, ltr559als_als_meas_rate_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xf8) | mode;
+    int     res    = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xf8) | mode;
     i2c_write_register(driver, ALS_MEAS_RATE, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
     return 0;
 }
 
-//integration time must be <= als_meas_rate
 
+// integration time must be <= als_meas_rate
 int ltr559als_set_als_integration_time(i2c_driver_t driver, ltr559als_als_integration_time_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xc7) | (mode << 3);
+    int     res    = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xc7) | (mode << 3);
     i2c_write_register(driver, ALS_MEAS_RATE, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, ALS_MEAS_RATE, &buffer, 1);
     return 0;
 }
 
 
 int ltr559als_set_als_data_gain(i2c_driver_t driver, ltr559als_als_gain_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, ALS_PS_STATUS, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0x8f) | (mode<<4);
+    int     res    = i2c_read_register(driver, ALS_PS_STATUS, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0x8f) | (mode << 4);
     i2c_write_register(driver, ALS_PS_STATUS, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, ALS_PS_STATUS, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, ALS_PS_STATUS, &buffer, 1);
     return 0;
 }
 
 
 int ltr559als_set_interrupt_polarity(i2c_driver_t driver, int polarity) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, INTERRUPT, &buffer, 1);
+    int     res    = i2c_read_register(driver, INTERRUPT, &buffer, 1);
     if (res)
         return res;
-    if (polarity == 0) 
+    if (polarity == 0)
         buffer = buffer & 0xfb;
     else if (polarity == 1)
         buffer = buffer | 0x4;
     return i2c_write_register(driver, INTERRUPT, &buffer, 1);
-
 }
 
-int ltr559als_set_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t als_mode, ltr559als_interrupt_persist_t ps_mode) {
+
+int ltr559als_set_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t als_mode,
+                                    ltr559als_interrupt_persist_t ps_mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0) | als_mode | (ps_mode<<4);
+    int     res    = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0) | als_mode | (ps_mode << 4);
     i2c_write_register(driver, INTERRUPT_PERSIST, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
     return 0;
 }
+
 
 // funzioni spezzate
-
 int ltr559als_set_ps_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xf) | (mode<<4);
+    int     res    = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xf) | (mode << 4);
     i2c_write_register(driver, INTERRUPT_PERSIST, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
     return 0;
 }
+
 
 int ltr559als_set_als_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t mode) {
     uint8_t buffer = 0;
-    int res = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
-    if (res) return res;
-    buffer=(buffer & 0xf0) | mode;
+    int     res    = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
+    if (res)
+        return res;
+    buffer = (buffer & 0xf0) | mode;
     i2c_write_register(driver, INTERRUPT_PERSIST, &buffer, 1);
-    buffer=0;
-    res = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
+    buffer = 0;
+    res    = i2c_read_register(driver, INTERRUPT_PERSIST, &buffer, 1);
     return 0;
 }
--- i2c_devices/LTR559ALS/ltr559als.h
+++ i2c_devices/LTR559ALS/ltr559als.h
@@ -4,49 +4,49 @@
 #include "../../i2c_common/i2c_common.h"
 #include <stdint.h>
 
-#define LTR_STATUS_ALS_DATA (1<<2)
+#define LTR_STATUS_ALS_DATA (1 << 2)
 
 typedef enum {
     INTERRUPT_DISABLED = 0,
-    INTERRUPT_PS=1,
-    INTERRUPT_ALS=2,
-    INTERRUPT_PS_ALS=3
+    INTERRUPT_PS       = 1,
+    INTERRUPT_ALS      = 2,
+    INTERRUPT_PS_ALS   = 3
 } ltr559als_interrupt_mode_t;
 
-//NEW 
+// NEW
 
 typedef enum {
-    ALS_GAIN_1X = 0,
-    ALS_GAIN_2X=1,
-    ALS_GAIN_4X=2,
-    ALS_GAIN_8X=3,
-    ALS_GAIN_48X=6,
-    ALS_GAIN_96X=7
-}ltr559als_als_gain_t;
+    ALS_GAIN_1X  = 0,
+    ALS_GAIN_2X  = 1,
+    ALS_GAIN_4X  = 2,
+    ALS_GAIN_8X  = 3,
+    ALS_GAIN_48X = 6,
+    ALS_GAIN_96X = 7
+} ltr559als_als_gain_t;
 
 typedef enum {
-    PS_MEAS_RATE_50MS = 0,
-    PS_MEAS_RATE_70MS = 1,
-    PS_MEAS_RATE_100MS = 2,
-    PS_MEAS_RATE_200MS = 3,
-    PS_MEAS_RATE_500MS = 4,
+    PS_MEAS_RATE_50MS   = 0,
+    PS_MEAS_RATE_70MS   = 1,
+    PS_MEAS_RATE_100MS  = 2,
+    PS_MEAS_RATE_200MS  = 3,
+    PS_MEAS_RATE_500MS  = 4,
     PS_MEAS_RATE_1000MS = 5,
     PS_MEAS_RATE_2000MS = 6,
-    PS_MEAS_RATE_10MS = 8
+    PS_MEAS_RATE_10MS   = 8
 } ltr559als_ps_meas_rate_t;
 
 typedef enum {
-    ALS_MEAS_RATE_50MS = 0,
-    ALS_MEAS_RATE_100MS = 1,
-    ALS_MEAS_RATE_200MS = 2,
-    ALS_MEAS_RATE_500MS = 3,
+    ALS_MEAS_RATE_50MS   = 0,
+    ALS_MEAS_RATE_100MS  = 1,
+    ALS_MEAS_RATE_200MS  = 2,
+    ALS_MEAS_RATE_500MS  = 3,
     ALS_MEAS_RATE_1000MS = 4,
     ALS_MEAS_RATE_2000MS = 5
 } ltr559als_als_meas_rate_t;
 
 typedef enum {
     ALS_INTEGRATION_TIME_100MS = 0,
-    ALS_INTEGRATION_TIME_50MS = 1,
+    ALS_INTEGRATION_TIME_50MS  = 1,
     ALS_INTEGRATION_TIME_200MS = 2,
     ALS_INTEGRATION_TIME_400MS = 3,
     ALS_INTEGRATION_TIME_150MS = 4,
@@ -56,22 +56,22 @@ typedef enum {
 } ltr559als_als_integration_time_t;
 
 typedef enum {
-    INTERRUPT_PERSIST_1=0,
-    INTERRUPT_PERSIST_2=1,
-    INTERRUPT_PERSIST_3=2,
-    INTERRUPT_PERSIST_4=3,
-    INTERRUPT_PERSIST_5=4,
-    INTERRUPT_PERSIST_6=5,
-    INTERRUPT_PERSIST_7=6,
-    INTERRUPT_PERSIST_8=7,
-    INTERRUPT_PERSIST_9=8,
-    INTERRUPT_PERSIST_10=9,
-    INTERRUPT_PERSIST_11=10,
-    INTERRUPT_PERSIST_12=11,
-    INTERRUPT_PERSIST_13=12,
-    INTERRUPT_PERSIST_14=13,
-    INTERRUPT_PERSIST_15=14,
-    INTERRUPT_PERSIST_16=15
+    INTERRUPT_PERSIST_1  = 0,
+    INTERRUPT_PERSIST_2  = 1,
+    INTERRUPT_PERSIST_3  = 2,
+    INTERRUPT_PERSIST_4  = 3,
+    INTERRUPT_PERSIST_5  = 4,
+    INTERRUPT_PERSIST_6  = 5,
+    INTERRUPT_PERSIST_7  = 6,
+    INTERRUPT_PERSIST_8  = 7,
+    INTERRUPT_PERSIST_9  = 8,
+    INTERRUPT_PERSIST_10 = 9,
+    INTERRUPT_PERSIST_11 = 10,
+    INTERRUPT_PERSIST_12 = 11,
+    INTERRUPT_PERSIST_13 = 12,
+    INTERRUPT_PERSIST_14 = 13,
+    INTERRUPT_PERSIST_15 = 14,
+    INTERRUPT_PERSIST_16 = 15
 } ltr559als_interrupt_persist_t;
 
 int ltr559als_read_info_id(i2c_driver_t driver, uint8_t *p_id, uint8_t *m_id);
@@ -84,7 +84,7 @@ int ltr559als_set_ps_thresold(i2c_driver_t driver, uint16_t low, uint16_t high);
 int ltr559als_read_status(i2c_driver_t driver, uint8_t *buffer);
 
 
-//NEW 
+// NEW
 int ltr559als_set_als_gain(i2c_driver_t driver, ltr559als_als_gain_t mode);
 int ltr559als_ps_saturation_indicator_en(i2c_driver_t driver, int enable);
 int ltr559als_set_ps_meas_rate(i2c_driver_t driver, ltr559als_ps_meas_rate_t mode);
@@ -92,9 +92,11 @@ int ltr559als_set_als_meas_rate(i2c_driver_t driver, ltr559als_als_meas_rate_t m
 int ltr559als_set_als_integration_time(i2c_driver_t driver, ltr559als_als_integration_time_t mode);
 int ltr559als_set_als_data_gain(i2c_driver_t driver, ltr559als_als_gain_t mode);
 int ltr559als_set_interrupt_polarity(i2c_driver_t driver, int polarity);
-int ltr559als_set_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t als_mode, ltr559als_interrupt_persist_t ps_mode);
+int ltr559als_set_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t als_mode,
+                                    ltr559als_interrupt_persist_t ps_mode);
 int ltr559als_set_ps_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t mode);
 int ltr559als_set_als_interrupt_persist(i2c_driver_t driver, ltr559als_interrupt_persist_t mode);
+int ltr559als_set_ps_offset(i2c_driver_t driver, uint16_t offset);
 
 int ltr559als_read_visible_spectrum(i2c_driver_t driver, uint16_t *light);
 #endif
