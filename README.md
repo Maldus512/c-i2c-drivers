a52782fd1138842779b2ce5c09939b6de94a1d42
Mattia Maldini <mattia512maldini@gmail.com>
2022-05-19 13:27:25 +0200

    documentation

--- README.md
+++ README.md
@@ -4,8 +4,26 @@ A collection of I2C drivers for various integrated circuits.
 Immediately pluggable as ESP-IDF component.
 
 
+## Zen
+
+This is a collection of drivers for various I2C integrated circuits. It includes memories, clocks and sensors.
+The code is hardware indipendent: all drivers rely on an interface-like struct (`i2c_driver_t`) with function pointers to interact with the underlying I2C network.
+
+This means that to actually use this library a few implementations are required. One must instance a struct as such:
+
+```c
+i2c_driver_t driver = {
+    .device_address = DEVICE_ADDRESS, // I2C device address
+    .i2c_transfer = my_i2c_transfer_function, // a function that writes bytes to the I2C network and reads the required response.
+    .ack_polling = my_ack_polling_function, // a function that performs ack polling. Useful for eeproms, not strictly required.
+    .delay_ms = my_delay_function, // a function that suspends execution for the required number of milliseconds. Only required for some drivers.
+    . arg = argument_pointer,   // a void pointer that is passed to i2c_transfer and ack_polling function. Serves as instantiation (e.g. when working with different I2C networks on the same device)
+    }:
+```
+
+The `i2c_port` folder contains some hardware implementations.
+
+
 ## TODO
 
-    - Add tests for al peripherals
-    - Complete ESP-IDF integration
-    - Add `SConscript` integration
+- Raise all code level up to standard (e.g. PIC ports)
--- i2c_devices/temperature/SHT3/sht3.h
+++ i2c_devices/temperature/SHT3/sht3.h
@@ -1,22 +1,61 @@
 #ifndef SHT3_H_INCLUDED
 #define SHT3_H_INCLUDED
 
-#define SHT3_DEFAULT_ADDRESS (0x44 << 1)
-
 #include "../../../i2c_common/i2c_common.h"
 
+
+#define SHT3_DEFAULT_ADDRESS (0x44 << 1)
+
+
+/**
+ * @brief Milliseconds needed to conclude a sensor read for each corresponding repeatibility level.
+ *
+ */
 #define SHT3_LOW_REPEATABILITY_MS_PERIOD    4
 #define SHT3_MEDIUM_REPEATABILITY_MS_PERIOD 6
 #define SHT3_HIGH_REPEATABILITY_MS_PERIOD   15
 
+
+/**
+ * @brief Repeatability values for sensor reads. Higher values will take more time and energy but will yield more
+ * accurate results.
+ *
+ */
 typedef enum {
     SHT3_LOW_REPEATABILITY = 0,
     SHT3_MEDIUM_REPEATABILITY,
     SHT3_HIGH_REPEATABILITY,
 } sht3_repeatability_t;
 
+
+/**
+ * @brief Start a single acquisition (to be later acquired)
+ *
+ * @param driver
+ * @param clock_stretching Whether to use clock stretching.
+ * @param repeatibility the repeatibility of the measurement.
+ * @return int
+ */
 int sht3_start_single_acquisition(i2c_driver_t driver, int clock_stretching, sht3_repeatability_t repeatibility);
+
+/**
+ * @brief Acquire a previously started sensor read. If it was not concluded yet the device does not respond; depending
+ * on the hardware abstraction layer implementation this may hang the call or fail it
+ *
+ * @param driver
+ * @param temperature Temperature read. May be NULL.
+ * @param humidity Humidity read. May be NULL.
+ * @return int
+ */
 int sht3_read_acquisition(i2c_driver_t driver, double *temperature, double *humidity);
+
+/**
+ * @brief Read the status register
+ *
+ * @param driver
+ * @param status Pointer where the status register content will be stored. Must not be NULL.
+ * @return int
+ */
 int sht3_read_status_register(i2c_driver_t driver, uint16_t *status);
 
 #endif
