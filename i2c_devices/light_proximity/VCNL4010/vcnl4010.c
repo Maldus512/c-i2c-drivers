312c40dc72c2ffd9445f4e77748926cb3d3b8bfc
Mattia Maldini <mattia512maldini@gmail.com>
2022-07-07 14:43:58 +0200

    Added VCNL4010 function

--- i2c_devices/light_proximity/VCNL4010/vcnl4010.c
+++ i2c_devices/light_proximity/VCNL4010/vcnl4010.c
@@ -1,4 +1,5 @@
 #include "vcnl4010.h"
+#include "esp_log.h"
 
 
 #define COMMAND_REGISTER                      0x80
@@ -26,8 +27,10 @@
 #define PROX_EN_BIT         1
 #define SELFTIMED_EN_BIT    0
 
+static const char         *TAG        = "vcnl4010";
 
 static int read_command_register(i2c_driver_t driver, uint8_t *command);
+static int write_generic_register(i2c_driver_t driver, uint8_t reg, uint8_t *data);
 static int command_register_update_bit(i2c_driver_t driver, uint8_t bit, uint8_t value);
 static int register_get_bit(i2c_driver_t driver, uint8_t address, uint8_t bit, uint8_t *value);
 
@@ -39,6 +42,30 @@ int vcnl4010_enable_operation(i2c_driver_t driver, uint8_t enable) {
 
 int vcnl4010_enable_periodic_proximity_measurement(i2c_driver_t driver, uint8_t enable) {
     return command_register_update_bit(driver, PROX_EN_BIT, enable > 0);
+}
+
+
+int vcnl4010_write_proximity_rate(i2c_driver_t driver, uint8_t value) {
+	uint8_t buffer = 0;
+	int     res    = i2c_read_register(driver, PROXIMITY_RATE_REGISTER, &buffer, 1);
+	if (value <= 7)
+	{
+		buffer = (buffer & 0xf0) | value;
+		return write_generic_register(driver, PROXIMITY_RATE_REGISTER, &buffer);
+	}
+	return 1;
+}
+
+int vcnl4010_write_proximity_current(i2c_driver_t driver, uint8_t value) {
+	uint8_t buffer = 0;
+	int     res    = i2c_read_register(driver, IR_LED_CURRENT_REGISTER, &buffer, 1);
+	if (value <= 20)
+	{
+		buffer = (buffer & 0xC0) | value;
+		ESP_LOGI(TAG, "Current sensor: %d", buffer);
+		return write_generic_register(driver, IR_LED_CURRENT_REGISTER, &buffer);
+	}
+	return 1;
 }
 
 
@@ -83,6 +110,11 @@ int vcnl4010_read_proximity_result(i2c_driver_t driver, uint16_t *result) {
 
 static int read_command_register(i2c_driver_t driver, uint8_t *command) {
     return i2c_read_register(driver, COMMAND_REGISTER, command, 1);
+}
+
+
+static int write_generic_register(i2c_driver_t driver, uint8_t reg, uint8_t *data) {
+    return i2c_write_register(driver, reg, data, 1);
 }
 
 
